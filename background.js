590bcfce9dc0761fe8e392294736a8981192d2a4
yiannisha <hadjiyiannis.3@gmail.com>
2022-02-20 16:34:17 +0200

    Fix minor syntacti bug

--- background.js
+++ background.js
@@ -4,14 +4,14 @@
 let highlightPressed = false;
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ "highlightPressed": highlightPressed }, function () {
+  chrome.storage.sync.set({ highlightPressed: highlightPressed }, function () {
     console.log("Initial highlightPressed value set to: " + highlightPressed);
   });
 });
 
 // set highlightPressed to false when tab changes
 chrome.tabs.onActivated.addListener(() => {
-  chrome.storage.sync.set({ "highlightPressed": false }, function () {
+  chrome.storage.sync.set({ highlightPressed: false }, function () {
     console.log("Tab changed. Set highlightPressed value to false.");
   });
 });
